d3c8cf87a64390e44304a12b45637206860386c9
kevke6 <kevke676@gmail.com>
2017-01-06 19:14:17 +0100

    created fight mission script so a player do a mission or fail it. Update character stats when lvl up. update stamina when new day. Calculation for each mission difficulty and more

--- game/app/js/pushData.php
+++ game/app/js/pushData.php
@@ -1,8 +1,6 @@
 <?php
-$data = json_decode(file_get_contents("php://input"));
+$statement = $_GET["statement"];
 include('config.php');
-$db = new DB();
 
-$sql = "INSERT INTO `blogs`(`title`,`description`)VALUES('$data->title','$data->description')";
-$data = $db->qryFire($sql);
-echo json_encode($data);
+$db = new DB();
+$data = $db->qryUpdate($statement);
